6b37fed69be0763be07271f9fa6d71a1b78dcfae
Syed Saquib Asghar <saq9516@gmail.com>
2024-03-16 00:53:31 +0530

    add max function

--- 03-Find the min-max from array/index.js
+++ 03-Find the min-max from array/index.js
@@ -1,6 +1,8 @@
 /*
     https://www.youtube.com/watch?v=rr9H9eIogyM&list=PLAx7-E_inM6EkgZkrujZvewiM_QZRU4A2&index=2
     find the min/max from the current array list
+    [11, 1, 34, 20, 5, 7, 9, 10, 0 ,-10] => -10 (min)
+     [11, 1, 34, 20, 5, 7, 9, 10, 0 ,-10] => 34 (max)
 */
 
 const originalArr = [11, 1, 34, 20, 5, 7, 9, 10, 0 ,-10]
@@ -10,4 +12,9 @@ Array.prototype.myMin = function(args){
     return min;
 }
 
+Array.prototype.myMax = function(args){
+    return Math.max(...args)
+}
+
 console.log(originalArr.myMin(originalArr))
+console.log(originalArr.myMax(originalArr))
