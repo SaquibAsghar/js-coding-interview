5547eec0735f258a3f0b7ae70e5f809e21cd0a4c
Syed Saquib Asghar <saq9516@gmail.com>
2024-03-15 22:05:06 +0530

    add practice question 1

---
+++ 01-Second Largest value /index.js
@@ -0,0 +1,35 @@
+/*Given an array of integers which can contain duplicate numbers as well, you have to find the second largest number in the array.
+
+Input: [8,2,1,1,2,4,5,8,7,7,9,0,8]
+Output: 8
+
+*/
+
+const originalArr = [8, 2, 1, 10, 20, 19, 5, 8, 19, 7, 9, 0, 8];
+
+function secondLargestVal(arr, cb) {
+  let tempArr = [];
+
+  arr.forEach((ele, i) => {
+    if (arr.indexOf(arr[i]) === i) {
+      tempArr.push(ele);
+    }
+  });
+  console.log(tempArr);
+  cb(tempArr);
+}
+
+secondLargestVal(originalArr, (arr) => {
+  let largest = arr[0],
+    secondLargest = -Infinity;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      secondLargest = largest;
+      largest = arr[i];
+    } else if (arr[i] < largest && arr[i] > secondLargest) {
+      secondLargest = arr[i];
+    }
+  }
+  console.log({ secondLargest, largest });
+  return secondLargest;
+});
