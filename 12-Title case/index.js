9638bda39981dddce187ee7d4cd8299c263f9ba7
Syed Saquib Asghar <saq9516@gmail.com>
2024-03-17 11:26:21 +0530

    remove the console log

--- 12-Title case/index.js
+++ 12-Title case/index.js
@@ -2,19 +2,19 @@
     title case problem. Cpitalise every starting word from the given sentence
 
     "hello world saquib" => "Hello World Saquib"
-*/ 
+*/
 
 const sentence = "hello world saquib";
 
 function capitaliseEveryWord(str) {
-    let capitaliseStatement = "";
-    str = str.split(" ");
-    str.forEach((element, i) => {
-        capitaliseStatement += element.slice(0, 1).toUpperCase() + element.slice(1) + " ";
-    });
+  let capitaliseStatement = "";
+  str = str.split(" ");
+  str.forEach((element, i) => {
+    capitaliseStatement +=
+      element.slice(0, 1).toUpperCase() + element.slice(1) + " ";
+  });
 
-    console.log(capitaliseStatement);
-    return capitaliseStatement;
+  return capitaliseStatement;
 }
 
 console.log(capitaliseEveryWord(sentence));
